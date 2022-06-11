8b13c21ff3c661851316c6a8f13d4707f0c7ce91
ernesto <ernestoleonidas@gmail.com>
2022-06-11 13:41:12 +0000

    Se añade script clase anterior

--- parque.sql
+++ parque.sql
@@ -1,6 +1,6 @@
-CREATE TABLE Cliente( 
+CREATE TABLE Clientes( 
     rut_cliente         NUMBER(11) NOT NULL, 
-    dv CHAR(1)          NOT NULL, 
+    dv                  CHAR(1) NOT NULL, 
     primer_nombre       VARCHAR2(250) NOT NULL, 
     segundo_nombre      VARCHAR2(250), 
     apellido_paterno    VARCHAR2(250) NOT NULL, 
@@ -11,8 +11,16 @@ CREATE TABLE Cliente(
     comuna_id           NUMBER NOT NULL 
 );
 
-CREATE TABLE Comuna( 
+ALTER TABLE Clientes ADD CONSTRAINT cliente_PK PRIMARY KEY (rut_cliente);
+
+CREATE TABLE Comunas( 
     id_comuna NUMBER NOT NULL, 
     nombre VARCHAR2(250) NOT NULL, 
     provincia_id NUMBER NOT NULL 
 );
+
+ALTER TABLE Comunas ADD CONSTRAINT comuna_PK PRIMARY KEY (id_comuna);
+
+ALTER TABLE Clientes ADD CONSTRAINT comuna_FK FOREIGN KEY (comuna_id) 
+    REFERENCES Comunas (id_comuna);
+    
