3cfaad0dd66eea874ddd5fe1940267a3101cec9b
ernesto <ernestoleonidas@gmail.com>
2022-06-14 23:56:48 +0000

    Clase del 14-06-22

--- parque.sql
+++ parque.sql
@@ -1,3 +1,4 @@
+-- Crear Tabla Clientes
 CREATE TABLE Clientes( 
     rut_cliente         NUMBER(11) NOT NULL, 
     dv                  CHAR(1) NOT NULL, 
@@ -11,53 +12,52 @@ CREATE TABLE Clientes(
     comuna_id           NUMBER NOT NULL 
 );
 
+--Añadir la PK a la Tabla Clientes
 ALTER TABLE Clientes ADD CONSTRAINT cliente_PK PRIMARY KEY (rut_cliente);
 
+--Creo Tabla Comunas
 CREATE TABLE Comunas( 
     id_comuna NUMBER NOT NULL, 
     nombre VARCHAR2(250) NOT NULL, 
     provincia_id NUMBER NOT NULL 
 );
 
+-- Añadir la PK a la tabla Comunas
 ALTER TABLE Comunas ADD CONSTRAINT comuna_PK PRIMARY KEY (id_comuna);
 
+-- Añadir la Clave Foranea o FK a la tabla Clientes
 ALTER TABLE Clientes ADD CONSTRAINT comuna_FK FOREIGN KEY (comuna_id) 
     REFERENCES Comunas (id_comuna);
-
-
-INSERT INTO Comunas VALUES(1, 'Arica', 1);
-
-INSERT INTO Clientes VALUES(1,'6','Alan','','Brito','','Su casa',555444,'su@correo.cl',1);
-
-SELECT 
-    cl.primer_nombre,
-    cl.apellido_paterno,
-    co.nombre AS "COMUNA"
-FROM Clientes cl
-    JOIN Comunas co ON (cl.comuna_id = co.id_comuna);
     
+-- Ingresar datos en la Comunas
+INSERT INTO Comunas VALUES (1, 'Arica', 1);
+INSERT INTO Comunas VALUES (2, 'Los Muermos', 1);
     
+-- Ingresar datos a la tabla Clientes
+INSERT INTO Clientes VALUES (123,'K','Alan','','Brito','','Su casa #123',5555,'alan@frito.cl',1);
+INSERT INTO Clientes VALUES (456,'K','Pedro','','Brito','','Su casa #123',6666,'pedro@brito.cl',1);
+
+INSERT INTO Clientes VALUES (124,'1','Maria','','Brito','','Su casa #123',7777,'maria@brito.cl',1);
+
+-- Select básico
+SELECT * FROM Clientes;
 SELECT * FROM Comunas;
 
-SELECT * FROM Clientes;
+-- Select Ordenado según columna
+SELECT * FROM Clientes ORDER BY primer_nombre DESC;
 
 SELECT 
-    co.nombre
-FROM Comunas co;
+    primer_nombre,
+    apellido_paterno,
+    comuna_id
+FROM Clientes;
 
-SELECT 
-    cl.rut_cliente,
-    cl.primer_nombre,
-    cl.apellido_paterno,
-    cl.comuna_id
-FROM Clientes cl;
 
+-- Select simple nivel 2
 SELECT 
-    cl.rut_cliente AS "Rut",
-    cl.primer_nombre,
-    cl.apellido_paterno,
-    cl.comuna_id,
-    co.nombre AS "Nombre Comuna"
-FROM Clientes cl 
+    cl.primer_nombre AS "Nombre",
+    cl.apellido_paterno AS "Apellido", 
+    co.nombre AS "Comuna"
+FROM Clientes cl
     JOIN Comunas co ON (cl.comuna_id = co.id_comuna)
 ;
